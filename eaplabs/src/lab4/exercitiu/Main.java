7389a64560421830b26f22287ecc9f60f4ef5a33
Madalina Zamfir <70132690+madalina-zamfir@users.noreply.github.com>
2021-03-15 17:36:14 +0200

    lab4 - initial commit

---
+++ eaplabs/src/lab4/exercitiu/Main.java
@@ -0,0 +1,28 @@
+package lab4.exercitiu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /* Sa se realizeze un form prin care userii se pot inscrie la un concurs: nume, prenume, oras, strada, nr, nr de telefon, mail
+        Se va crea un serviciu care valideaza corectitudinea datelor introduse:
+            * numele si prenumele nu trebuie sa contina caractere speciale (inafara de " " si "-") si sa nu contina mai mult de 50 de caractere
+            * nr de telefon (mobil) valid
+            * adresa de email valida
+            * nr - maxim 3 caractere si sa nu inceapa cu 0
+            * orasul exista in "baza de date" - un array
+
+        Se va crea un serviciu realizeaza cateva transformari:
+               * prima litera din nume trebuie sa fie upper case, restul lower case
+               * email - lower case
+               * se va "mappa" la tipul de date Application: contact, address, name
+
+        Se va crea un serviciu care:
+                * salveaza aplicantii in baza de date(un array)
+                * are o metoda care returneaza toti aplicantii sortati dupa nume
+                * are o metoda care returneaza toti aplicantii dintr un anumit oras dat ca parametru
+
+        * se vor suprascrie metodele toString din fiecare clasa DTO (Data transfer object)/ POJO (Plain old java object)
+        */
+
+    }
+}
---
+++ eaplabs/src/lab4/mini_exercitiu_string/Main.java
@@ -0,0 +1,18 @@
+package lab4.mini_exercitiu_string;
+
+public class Main {
+
+    // StringBuilder si StringBuffer
+    public static void main(String[] args) {
+        // sa se inverseze ordinea cuvintelor, expectedOutput = "mere are Ana"
+        String input = "Ana are mere";
+
+        // sa se verifice daca un sir de caractere e polindrom
+
+        // sa se inlocuiasca "journey" cu "walk"
+        String title = "A journey of a thousand miles begins with a single step";
+
+        //sa se ascunda parola cu *****
+        String command = "curl -u mzamfir:superSecret -X GET http://something.ro";
+    }
+}
